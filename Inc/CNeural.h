7fa45885f1ce0ad915d06192e6151e0cb7bd65dd
JingyuChen-Personal <164629737+JingyuChen-Personal@users.noreply.github.com>
2025-08-20 03:18:25 +0800

    Add files via upload

--- Inc/CNeural.h
+++ Inc/CNeural.h
@@ -3,26 +3,30 @@
 
 // #define ONLY_NECCESSARY_CHECK
 
-// #define  USE_CBLAS_API
+// #define USE_OPENBLAS
+// #define USE_ONEMKL
 
-
-#ifdef  USE_CBLAS_API
-
-#include "cblas.h"
-
+#if defined USE_ONEMKL
+    #include "mkl_cblas.h"
+    #define  USE_CBLAS_API
+typedef MKL_INT cblas_int;
 #endif
 
-typedef enum
-{
-	CNeural_Success,
-	CNeural_Error_NullPointer,
-	CNeural_Error_InvalidParameter,
-	CNeural_Error_AllocationFailed
+#if defined USE_OPENBLAS
+    #include "cblas.h"
+    #define USE_CBLAS_API
+    typedef blasint cblasint;
+#endif
+
+typedef enum {
+    CNeural_Success,
+    CNeural_Error_NullPointer,
+    CNeural_Error_InvalidParameter,
+    CNeural_Error_AllocationFailed
 } CNeural_Status;
 
 
-typedef enum
-{
+typedef enum {
     CNeural_Reduction_None,
     CNeural_Reduction_Mean,
     CNeural_Reduction_Sum
